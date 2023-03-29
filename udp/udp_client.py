68b71af2614206a7007a4ef2dc32e9b07807ab13
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-03-29 15:50:59 -0300

    Feat: commenting udp_client

--- udp/udp_client.py
+++ udp/udp_client.py
@@ -1,10 +1,12 @@
 #Rodar depois do udp_server.py
 import socket
-
+#Define o endereço IP local e a porta do servidor
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
+#Define a mensagem que será enviada ao servidor
 MESSAGE = "Hello, Server!"
-
+#Cria um objeto de socket UDP
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+#Envia a mensagem definida anteriormente ao servidor no endereço e porta especificados. O método encode() é usado para converter a mensagem para bytes antes de enviá-la.
